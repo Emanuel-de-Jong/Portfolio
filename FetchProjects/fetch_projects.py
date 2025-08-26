7571241eb137537ad6fd03a887a79da2798df629
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-08-26 16:38:44 +0200

    Logger and log file

--- FetchProjects/fetch_projects.py
+++ FetchProjects/fetch_projects.py
@@ -1,5 +1,7 @@
 import os
 import re
+import sys
+import logging
 import requests
 import subprocess
 from bs4 import BeautifulSoup
@@ -20,13 +22,38 @@ class Project:
         self.img_paths = []
         self.p_langs = []
 
+def setup_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler("log.log", encoding="utf-8", mode="a")
+    file_handler.setFormatter(logging.Formatter(
+        "[{asctime}][{levelname}]: {message}",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        style="{"
+    ))
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter(
+        "[{levelname}]: {message}",
+        style="{"
+    ))
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    return logger
+
+logger = setup_logger()
+
 def fetch_projects():
+    logger.info("Fetching projects...")
+
     projects = repos_to_projects()
     js = projects_to_js(projects)
     with open("../js/projects.js", "w") as f:
         f.write(js)
     
-    print("Done!")
+    logger.info("Done!")
 
 def repos_to_projects():
     projects = []
@@ -43,7 +70,7 @@ def repos_to_projects():
             if github_page == None:
                 continue
 
-            print(f"=== {repo_name} ===")
+            logger.info(f"=== {repo_name} ===")
             project = Project(repo_name)
 
             project.made_for = "Hobby"
@@ -76,7 +103,7 @@ def get_github_page(repo_name):
             if ">Public</span>" in html:
                 github_page = html
     except Exception as e:
-        print(e)
+        logger.error(e, exc_info=True)
 
     return github_page
 
@@ -86,12 +113,13 @@ def set_data_from_github_page(project, github_page):
     if len(project.p_langs) == 0:
         html_langs_header = soup.find('h2', string='Languages')
         if not html_langs_header:
-            print("No 'Languages' header in the HTML.")
+            logger.warning("No 'Languages' header in the HTML.")
             return
         
         html_langs = html_langs_header.find_next('ul')
         for html_lang in html_langs.find_all('li'):
-            lang_percentage = float(html_lang.find('span', string=lambda t: t and '%' in t).text.strip('%'))
+            html_percentage = html_lang.find('span', string=lambda t: t and '%' in t)
+            lang_percentage = float(html_percentage.text.strip('%').strip())
             if lang_percentage < 10:
                 continue
 
@@ -99,7 +127,7 @@ def set_data_from_github_page(project, github_page):
             project.p_langs.append(lang_name)
         
         if len(project.p_langs) == 0:
-            print("Could not extract languages from the HTML.")
+            logger.warning("Could not extract languages from the HTML.")
 
 def set_data_from_local_clone(project, repo_path):
     set_screenshot_paths(project, repo_path)
@@ -109,7 +137,7 @@ def set_data_from_local_clone(project, repo_path):
 def set_screenshot_paths(project, repo_path):
     screenshots_path = os.path.join(repo_path, "Screenshots")
     if not os.path.exists(screenshots_path):
-        # print("No 'Screenshots' directory.")
+        # logger.warning("No 'Screenshots' directory.")
         return
     
     for filename in os.listdir(screenshots_path):
@@ -117,7 +145,7 @@ def set_screenshot_paths(project, repo_path):
             project.img_paths.append(f"Screenshots/{filename}")
     
     # if len(project.img_paths) == 0:
-    #     print("No PNGs in the 'Screenshots' directory.")
+    #     logger.warning("No PNGs in the 'Screenshots' directory.")
 
 def set_main_branch_name(project, repo_path):
     git_dir_path = os.path.join(repo_path, ".git")
@@ -132,17 +160,17 @@ def set_main_branch_name(project, repo_path):
         match = re.search(r'^\s*HEAD branch:\s*(\S+)', remote_show_output, re.MULTILINE)
         
         if not match:
-            print("Could not extract the branch name.")
+            logger.warning("Could not extract the branch name.")
             return
 
         project.branch = match.group(1)
     except Exception as e:
-        print(e)
+        logger.error(e, exc_info=True)
 
 def set_data_from_readme(project, repo_path):
     readme_path = os.path.join(repo_path, "README.md")
     if not os.path.exists(readme_path):
-        print("No 'README.md' file.")
+        logger.warning("No 'README.md' file.")
         return
     
     with open(readme_path, 'r', encoding='utf-8') as f:
@@ -151,6 +179,10 @@ def set_data_from_readme(project, repo_path):
     project.name = readme.split("\n")[0].replace("#", "").strip()
 
     description = readme.split("\n")[1].strip().replace("`", "")
+    
+    # Change markdown link syntax `[text](url)`` into just the text
+    description = re.sub(r'\[([^\]]+)\]\([^)]+\)', r'\1', description)
+
     # Max 220 characters
     description = description[:220]
 
@@ -158,9 +190,6 @@ def set_data_from_readme(project, repo_path):
         last_terminator = max(description.rfind('.'), description.rfind('!'), description.rfind('?'))
         if last_terminator != -1:
             description = description[:last_terminator + 1]
-    
-    # Change markdown link syntax `[text](url)`` into just the text
-    description = re.sub(r'\[([^\]]+)\]\([^)]+\)', r'\1', description)
 
     project.description = description
 
@@ -209,4 +238,8 @@ def project_to_js(project):
     return js
 
 if __name__ == "__main__":
-    fetch_projects()
+    try:
+        fetch_projects()
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        sys.exit(1)
